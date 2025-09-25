2457f8f1ed829c1d6e8deef339794ca35c3a9d67
sajidammar <servialsaro@gmail.com>
2025-09-25 18:21:22 +0300

    order the folders and files to preparing the project

---
+++ lib/screens/main_screens/drowerscreen.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class sidebarbutton extends StatelessWidget {
+  sidebarbutton(IconData icon, String label, {super.key}) {
+    icon = icon;
+    this.label = label;
+  }
+
+  IconData? icon;
+  String? label;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return TextButton.icon(
+      onPressed: () {},
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.all(16),
+        foregroundColor: Colors.white,
+        alignment: Alignment.centerLeft,
+      ),
+      icon: Icon(icon, color: Colors.tealAccent),
+      label: Text(label!),
+    );
+  }
+
+  // (IconData icon, String label)
+}
+
+// ignore: must_be_immutable
+class BuildMainArea extends StatelessWidget {
+  BuildMainArea({super.key, bool showDrawerButtonn = true}) {
+    this.showDrawerButton = showDrawerButtonn;
+  }
+  bool? showDrawerButton;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CryptoCyber'),
+        leading:
+            showDrawerButton!
+                ? Builder(
+                  builder:
+                      (context) => IconButton(
+                        icon: const Icon(Icons.menu),
+                        onPressed: () => Scaffold.of(context).openDrawer(),
+                      ),
+                )
+                : null,
+      ),
+      drawer:
+          showDrawerButton!
+              ? Drawer(
+                child: Container(
+                  width: 220,
+                  color: const Color(0xFF101622),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      DrawerHeader(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Icon(
+                              Icons.lock_outline,
+                              size: 48,
+                              color: Colors.tealAccent,
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              'CryptoCyber Menu',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      sidebarbutton(Icons.vpn_key, 'Encryption'),
+                      sidebarbutton(Icons.lock_open, 'Decryption'),
+                      sidebarbutton(Icons.analytics, 'Analysis'),
+                      sidebarbutton(Icons.settings, 'Settings'),
+                    ],
+                  ),
+                ),
+              )
+              : null,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(flex: 1, child: LogoPanel()),
+            const SizedBox(height: 16),
+            Expanded(flex: 1, child: DetailsPanel()),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LogoPanel extends StatelessWidget {
+  const LogoPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      color: const Color.fromARGB(255, 33, 35, 40),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Stack(
+          fit: StackFit.expand,
+          children: [Image.asset('images/mainscreen.jpg')],
+        ),
+      ),
+    );
+  }
+}
+
+class DetailsPanel extends StatelessWidget {
+  const DetailsPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      color: const Color.fromARGB(255, 33, 35, 40),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                const Text(
+                  'Dtails CryptoCyber',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.menu, color: Colors.tealAccent),
+                  onPressed: () {},
+                  tooltip: 'Show The Service In This Program',
+                ),
+              ],
+            ),
+            const Divider(),
+            const Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  spacing: 50,
+                  children: [
+                    SizedBox(width: double.infinity),
+                    Icon(size: 100, Icons.enhanced_encryption),
+                    Icon(size: 100, Icons.vpn_key),
+                    Icon(size: 100, Icons.analytics_outlined),
+                    Icon(size: 100, Icons.no_encryption_gmailerrorred),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
